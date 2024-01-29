6d783634df0ab2cb3b24e80304be095173321871
kayano44 <156571686+kayano44@users.noreply.github.com>
2024-01-29 17:10:37 +0900

    フロア会議室表示

--- signage/js/floor-on-off.js
+++ signage/js/floor-on-off.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
-    // フロア画像のホバーイベント
+    
+    // フロア画像のホバーイベントで画像変更とフロア表示変更
     $('.container_floor img').hover(
-      function() {
         // マウスオーバー時の処理
-        $(this).data('original-src', $(this).attr('src'));  // 元の画像を記憶
-        $(this).attr('src', $(this).data('hover-src'));     // ホバー画像に変更
-      },
-      function() {
+        function() {
+            // 画像をホバー画像に変更
+            $(this).data('original-src', $(this).attr('src')); // 元の画像を記憶
+            $(this).attr('src', $(this).data('hover-src')); // ホバー画像に変更
+            
+            // h2タグのテキストを変更
+            var floorNumber = $(this).attr('alt').charAt(0); // フロア番号を取得
+            $(".floor-info h2").text(floorNumber + 'F会議室フロア'); // 内容を変更
+        },
+        
         // マウスアウト時の処理
-        $(this).attr('src', $(this).data('original-src'));  // 元の画像に戻す
-      }
+        function() {
+            // 画像を元の画像に戻す
+            $(this).attr('src', $(this).data('original-src')); // 元の画像に戻す
+            
+            // h2タグのテキストを元に戻す
+            $(".floor-info h2").text(""); // 元の内容に戻す
+        }
     );
-  
+
     // フロア画像のクリックイベント
     $('.container_floor img').click(function() {
-      // フロア画像のalt属性からフロア番号を取得
-      var floorNumber = $(this).attr('alt').charAt(0);
-      // 対応するフロアのHTMLファイルのパスを構築
-      var floorPath = floorNumber + 'st_floor.html';
-      // HTMLファイルへの移動
-      window.location.href = floorPath;
+        // フロア画像のalt属性からフロア番号を取得
+        var floorNumber = $(this).attr('alt').charAt(0);
+        // 対応するフロアのHTMLファイルのパスを構築
+        var floorPath = floorNumber + 'st_floor.html';
+        // HTMLファイルへの移動
+        window.location.href = floorPath;
     });
-  });
+});
