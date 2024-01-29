dbbf5b22809a0c928b9cae193023aea4687a9322
ytk6 <takuya14816359@yahoo.co.jp>
2024-01-30 00:37:04 +0900

    レイアウト調整
    会議室の名称の整合、スイッチサイズの調整等行いました。

--- signage/js/sw-app.js
+++ signage/js/sw-app.js
@@ -1,27 +1,53 @@
 //ON/OFFの文字の切替を設定
-//会議室1
-const acCheckboxR1 = document.getElementById('ac-sw-r1');
-acCheckboxR1.addEventListener('click', () => {
-  const acTitleR1 = document.querySelector('.ac-title-r1');
-  acTitleR1.textContent = acCheckboxR1.checked ? 'ON' : 'OFF';
+//会議室A
+const acCheckboxRA = document.getElementById('ac-sw-rA');
+acCheckboxRA.addEventListener('click', () => {
+  const acTitleRA = document.querySelector('.ac-title-rA');
+  acTitleRA.textContent = acCheckboxRA.checked ? 'ON' : 'OFF';
 });
 
-const lightCheckboxR1 = document.getElementById('light-sw-r1');
-lightCheckboxR1.addEventListener('click', () => {
-  const lightTitleR1 = document.querySelector('.light-title-r1');
-  lightTitleR1.textContent = lightCheckboxR1.checked ? 'ON' : 'OFF';
+const lightCheckboxRA = document.getElementById('light-sw-rA');
+lightCheckboxRA.addEventListener('click', () => {
+  const lightTitleRA = document.querySelector('.light-title-rA');
+  lightTitleRA.textContent = lightCheckboxRA.checked ? 'ON' : 'OFF';
 });
 
-//会議室2
-const acCheckboxR2 = document.getElementById('ac-sw-r2');
-acCheckboxR2.addEventListener('click', () => {
-  const acTitleR2 = document.querySelector('.ac-title-r2');
-  acTitleR2.textContent = acCheckboxR2.checked ? 'ON' : 'OFF';
+//会議室B
+const acCheckboxRB = document.getElementById('ac-sw-rB');
+acCheckboxRB.addEventListener('click', () => {
+  const acTitleRB = document.querySelector('.ac-title-rB');
+  acTitleRB.textContent = acCheckboxRB.checked ? 'ON' : 'OFF';
 });
 
-const lightCheckboxR2 = document.getElementById('light-sw-r2');
-lightCheckboxR2.addEventListener('click', () => {
-  const lightTitleR2 = document.querySelector('.light-title-r2');
-  lightTitleR2.textContent = lightCheckboxR2.checked ? 'ON' : 'OFF';
+const lightCheckboxRB = document.getElementById('light-sw-rB');
+lightCheckboxRB.addEventListener('click', () => {
+  const lightTitleRB = document.querySelector('.light-title-rB');
+  lightTitleRB.textContent = lightCheckboxRB.checked ? 'ON' : 'OFF';
+});
+
+//会議室C
+const acCheckboxRC = document.getElementById('ac-sw-rC');
+acCheckboxRC.addEventListener('click', () => {
+  const acTitleRC = document.querySelector('.ac-title-rC');
+  acTitleRC.textContent = acCheckboxRC.checked ? 'ON' : 'OFF';
+});
+
+const lightCheckboxRC = document.getElementById('light-sw-rC');
+lightCheckboxRC.addEventListener('click', () => {
+  const lightTitleRC = document.querySelector('.light-title-rC');
+  lightTitleRC.textContent = lightCheckboxRC.checked ? 'ON' : 'OFF';
+});
+
+//会議室D
+const acCheckboxRD = document.getElementById('ac-sw-rD');
+acCheckboxRD.addEventListener('click', () => {
+  const acTitleRD = document.querySelector('.ac-title-rD');
+  acTitleRD.textContent = acCheckboxRD.checked ? 'ON' : 'OFF';
+});
+
+const lightCheckboxRD = document.getElementById('light-sw-rD');
+lightCheckboxRD.addEventListener('click', () => {
+  const lightTitleRD = document.querySelector('.light-title-rD');
+  lightTitleRD.textContent = lightCheckboxRD.checked ? 'ON' : 'OFF';
 });
 
