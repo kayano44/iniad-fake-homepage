5a9eb12e9a4761645bcef6fffff22f8df07b1137
kayano44 <156571686+kayano44@users.noreply.github.com>
2024-01-29 16:56:38 +0900

    floorにリンクを埋め込み

--- signage/js/floor-on-off.js
+++ signage/js/floor-on-off.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $('img').hover(
+    // フロア画像のホバーイベント
+    $('.container_floor img').hover(
       function() {
         // マウスオーバー時の処理
         $(this).data('original-src', $(this).attr('src'));  // 元の画像を記憶
@@ -10,4 +11,14 @@ $(document).ready(function() {
         $(this).attr('src', $(this).data('original-src'));  // 元の画像に戻す
       }
     );
+  
+    // フロア画像のクリックイベント
+    $('.container_floor img').click(function() {
+      // フロア画像のalt属性からフロア番号を取得
+      var floorNumber = $(this).attr('alt').charAt(0);
+      // 対応するフロアのHTMLファイルのパスを構築
+      var floorPath = floorNumber + 'st_floor.html';
+      // HTMLファイルへの移動
+      window.location.href = floorPath;
+    });
   });
